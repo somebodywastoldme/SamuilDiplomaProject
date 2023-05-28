084ad14fa9c69ca7fbfacd6eb629a1f8b7909085
Samuil <suhomlinsam@gmail.com>
2023-05-28 21:10:39 +0300

    Add initial migration

--- prisma/migrations/20230413194033_initial_14/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Document" ALTER COLUMN "id" SET DEFAULT serial,
-ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "Document_id_seq";
-
--- AlterTable
-ALTER TABLE "Submission" ALTER COLUMN "id" SET DEFAULT serial,
-ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "Submission_id_seq";
---
+++ prisma/migrations/20230528175051_initial_migration/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "file" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
